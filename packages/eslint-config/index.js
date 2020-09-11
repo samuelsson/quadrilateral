3417deb1db0973464aaa5f5398c2d8ee5bfa3328
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2020-09-11 15:27:56 +0200

    fix: 🐛 add new rule for fixing no-use-before-define
    This could be a configuration that will be fixed in a new version of
    typescript eslint plugin. Currently this needs to be added. More can be
    read
    [https://github.com/typescript-eslint/typescript-eslint/issues/2502#issuecomment-689595020](here)
    if we can remove thie later.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -32,6 +32,8 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'import/prefer-default-export': 0,
     'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'no-use-before-define': [0],
+    '@typescript-eslint/no-use-before-define': [1],
   },
   env: {
     es6: true,

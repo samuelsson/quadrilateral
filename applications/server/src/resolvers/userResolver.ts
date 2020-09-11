eebd25a514b1abf53e746d9fbc7c0eaf569a2596
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2020-09-11 14:41:54 +0200

    feat: 🎸 add basic login functionality

--- applications/server/src/resolvers/userResolver.ts
+++ applications/server/src/resolvers/userResolver.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import argon2 from 'argon2';
+import { ValidationError } from 'class-validator';
 import { User, UserModel } from '../models/User';
 import UserInput from './userInput';
 import { generateJwtToken } from '../helpers/auth';
@@ -9,9 +10,23 @@ import { generateJwtToken } from '../helpers/auth';
 class UserResolver {
   private readonly userModel = UserModel;
 
-  @Query((returns) => User, { nullable: false })
-  async userByID(@Arg('id') id: string): Promise<User> {
-    return this.userModel.findById({ _id: id });
+  @Query((returns) => String)
+  async login(@Arg('input') { email, password }: UserInput): Promise<String> {
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      // TODO: Throw correct error for no user found
+      throw new ValidationError();
+    }
+
+    const validPassword = await argon2.verify(user.password, password);
+
+    if (!validPassword) {
+      // TODO: Throw correct error for invalid password
+      throw new ValidationError();
+    }
+
+    return generateJwtToken(user);
   }
 
   @Mutation((returns) => String)

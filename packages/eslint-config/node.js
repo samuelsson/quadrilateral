d13e9e223c90641c2bc078f05a682b0e05c9e252
Erik Samuelsson <1119465+samuelsson@users.noreply.github.com>
2020-09-15 17:20:53 +0200

    fix: 🐛 disable eslint no-shadow base rule
    The base `no-shadow` rule is not playing nice with its TypeScript
    equivalent. Disabling the base one and only keeping the TypeScript rule
    removes the false negatives.

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -19,6 +19,8 @@ module.exports = {
     ],
     'import/prefer-default-export': 0,
     'no-console': 0,
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
   },
   settings: {
     'import/resolver': {
